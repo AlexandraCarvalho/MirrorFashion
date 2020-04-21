5661aa4df5ec1c1ee40664f311520796319ca3a5
Alexandra <alexandracarvalho0202@gmail.com>
2020-04-21 17:08:49 -0300

    alteração de rodape

--- index.php
+++ index.php
@@ -169,17 +169,7 @@
             </section>
         </div> 
         <!-- fim .container . paineis-->
-        <footer>
-            <div class="container">
-                <img src="img/logo-rodape.png" alt="Logo da Mirror Fashion">
-
-                <ul class="social">
-                    <li><a href="http://facebook.com/mirrorfashion">Facebook</a></li>
-                    <li><a href="http://twitter.com/mirrorfashion">Twitter</a></li>
-                    <li><a href="http://google.com/mirrorfashion">Google</a></li>
-                </ul>
-            </div>
-        </footer>
+            <!--anteriormente footer-->
         <script type="text/javascript" src="js/jquery.js"></script>
         <script type="text/javascript" src="js/bootstrap.js"></script>
         <script type="text/javascript" src="js/banner.js"></script>
