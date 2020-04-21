d219d04e6335d92c5e99b92f4274bddae895b3b4
Alexandra <alexandracarvalho0202@gmail.com>
2020-04-21 17:24:53 -0300

    remoção do rodapé duplicado

--- produto.php
+++ produto.php
@@ -83,17 +83,6 @@
             </table>
     </div>
 </div>
-    <footer>
-        <div class="container">
-            <img src="img/logo-rodape.png" alt="Logo da Mirror Fashion">
-
-            <ul class="social">
-                <li><a href="http://facebook.com/mirrorfashion">Facebook</a></li>
-                <li><a href="http://twitter.com/mirrorfashion">Twitter</a></li>
-                <li><a href="http://google.com/mirrorfashion">Google</a></li>
-            </ul>
-        </div>
-    </footer>
     <script type="text/javascript" src="js/produto.js"></script>
     <?php include("rodape.php");?>
 </body>
