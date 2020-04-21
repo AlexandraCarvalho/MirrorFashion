4873698f3785f2a08fef224110f33a16c57c72ba
Alexandra <alexandracarvalho0202@gmail.com>
2020-04-21 17:59:44 -0300

    adicação do arquivo

---
+++ checkout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Checkout Mirror Fashion</title>
+</head>
+<body>
+    
+    <h1>Otima escolha!</h1>
+    <p>Obrigada por comprar na Mirror Fashion!
+        Preencha seus dados para efetivar a compra.</p>
+
+   
+</body>
+</html>
